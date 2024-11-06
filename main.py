fe38380d14f460d8f5539f095877ce2a95c95367
Roman1456 <151393377+Roman1456@users.noreply.github.com>
2024-11-06 13:05:11 +0300

    hnjhnhnjh

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
 df = pd.read_csv('Space_Corrected (1).csv')
+df.info()
 
+df[" Rocket "]=df[' Rocket '].fillna(0)
+df[" Rocket "]= pd.to_numeric(df[" Rocket "], errors='coerce' )
 print(df['Rocket'].max())
